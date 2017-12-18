a62192e401fecfded2c64cb35b3d2205cdc80b43
alexandrapopova <34661673+alexandrapopova@users.noreply.github.com>
2017-12-19 02:28:04 +0300

    )

---
+++ yandex/yandex/Translator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace yandex
+{
+    class Translator
+    {
+        public string Translate(string s)
+        {
+            string line = string.Empty;
+            Translation a = null;
+            if (!string.IsNullOrEmpty(s))
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171217T153500Z.e982aab086711b47.9ad3b1d68ea1498dc93518918e939728b1afd999&text=" + WebUtility.UrlEncode(s)
+                        + "&lang=ru-en");
+
+                    WebResponse response = request.GetResponse();
+                    using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                    {
+                        line = stream.ReadToEnd();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Can't load result");
+                    return null;
+                }
+                try
+                {
+                    a = JsonConvert.DeserializeObject<Translation>(line);
+                }
+                catch
+                {
+                    MessageBox.Show("Can't parse answer");
+                    return null;
+                }
+            }
+            return a.text[0];
+        }
+    }
+
+    class Translation
+    {
+        public string[] text { get; set; }
+    }
+}
---
+++ yandex/yandex/viewmodel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace yandex
+{
+    class viewmodel : INotifyPropertyChanged
+    {
+        private Translator tr;
+        private string _origin;
+        public string Origin
+        {
+            get { return _origin; }
+            set
+            {
+                _origin = value;
+                OnPropertyChanged("Origin");
+            }
+        }
+        private string _translation;
+        public string Translation
+        {
+            get { return _translation; }
+            set
+            {
+                _translation = value;
+                OnPropertyChanged("Translation");
+            }
+        }
+        public Command Translate { get; set; } = new Command();
+        public viewmodel()
+        {
+            Translate.Func = TrText;
+        }
+        private async void TrText(object parameter)
+        {
+            Translator tr = new Translator();
+            Translation = tr.Translate(Origin);
+            SqlConnectionStringBuilder c = new SqlConnectionStringBuilder();
+            c.DataSource = ".\\SQLEXPRESS";
+            c.InitialCatalog = "Translator";
+            c.IntegratedSecurity = true;
+            using (SqlConnection dbase = new SqlConnection(c.ConnectionString))
+            {
+                SqlCommand comm = new SqlCommand("insert into Recent(origin, translation) values (@in, @out)", dbase);
+                comm.Parameters.AddWithValue("@in", Origin);
+                comm.Parameters.AddWithValue("@out", Translation);
+                try
+                {
+                    await dbase.OpenAsync();
+                    await comm.ExecuteNonQueryAsync();
+                    dbase.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Database is inaccessible");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+    }
+    class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public Action<object> Func { get; set; }
+        public void Execute(object parameter)
+        {
+            Func(parameter);
+        }
+    }
+}
